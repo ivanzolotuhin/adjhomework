244e6e20ddc825d8f4f486375c7d41b38ab5107d
Ivan Zolotuhhin <ivan.zolotuhhin@informa.com>
2021-06-02 21:57:51 +0300

    Wait helm

--- cicd.sh
+++ cicd.sh
@@ -4,6 +4,6 @@
 echo 'BUILDING DOCKER CONTAINER'
 docker build -t adjust:latest .
 echo 'DEPLOYING TO K8S WITH HELM'
-helm upgrade --install adjust -f values.yaml helm_chart
+helm upgrade --install adjust --wait -f values.yaml helm_chart
 echo 'TEST'
 kubectl run -i --tty test --image=adjust:latest --rm --image-pull-policy='Never' --restart=Never --command -- curl adjust.default.svc.cluster.local | grep 'Well, hello there' && echo 'SITE WORKS'
